acd020cf84220d4cb99d96088e03ffc549d2d825
Bram Donovan <71541070+TechSavvyOutdoorsman@users.noreply.github.com>
2021-07-14 01:00:52 -0600

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('moongose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// BodyParser Middleware
+
+app.use(bodyParser.json());
+
+// DB Config
+
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDB Connected..."))
+    .catch(err, () => console.log(err));
+
+    const port = process.env.PORT || 5000;
+
+    app.listen(port, () => console.log(`Server started on port ${port}`));
+
+    
